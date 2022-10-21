f4aca75ab4c00263786cd608030132270d5b20c7
syjsdev <romo.jorge126@gmail.com>
2022-10-20 22:38:27 -0500

    update the color config

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,28 +8,11 @@ module.exports = {
     },
     extend: {
       colors: {
-        black: "rgb(0, 0, 0)",
-        "jungle-green": {
-          700: "rgb(28, 31, 55)",
-        },
-        red: { 700: "rgb(185, 28, 28)" },
-        purple: { 700: "rgb(109, 40, 217)" },
-        "light-blue": {
-          600: "rgb(9, 129, 195)",
-        },
-        blue: {
-          500: "rgb(59, 130, 246)",
-        },
-        orange: { 500: "rgb(249, 115, 22)" },
-        teal: { 500: "rgb(20, 184, 166, 1)" },
-        gray: {
-          50: "rgb(249, 250, 251)",
-          100: "rgb(246, 248, 250)",
-          150: "rgb(241, 242, 250)",
-          300: "rgb(209, 213, 219)",
-          900: "rgb(55, 65, 81)",
-        },
-        white: "rgb(255, 255, 255)",
+        "jungle-green-dark": "rgba(28, 31, 55, 1)",
+        charcoal: "rgba(55, 65, 81, 1)",
+        "ghost-white": "rgba(246, 248, 250, 1)",
+        munsell: "rgba(241, 242, 250, 1)",
+        "light-blue": "rgba(9, 129, 195, 1)",
       },
     },
   },
